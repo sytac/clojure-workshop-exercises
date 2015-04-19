b52727b4ad9cccb2be997a5a2bcfaedff47a0589
Carlo Sciolla <skuro@skuro.tk>
2015-04-19 15:35:48 +0200

    Better documentation of the roman numerals kata

--- test/tdd/romans.clj
+++ test/tdd/romans.clj
@@ -9,7 +9,40 @@
 ;                                                                ;
 ;----------------------------------------------------------------;
 
+; Quick recap on Roman numerals:
+;
+; I => 1
+; V => 5
+; X => 10
+; L => 50
+; C => 100
+; D => 500
+; M => 100
+;
+; Symbols are places next to each other in value order, then summed up:
+;
+; MDCLXVI => 1666
+;
+; There can be only up to three repeated symbols in a row:
+;
+; XI     => 11
+; XII    => 12
+; XIII   => 13
+; XIIII  => WRONG!
+;
+; When a symbol is preceded by a symbol of a lesser value, you subtract it:
+;
+; IX => 8
+;
+; There can be only up to one subtractive symbol in a row:
+;
+; IX   => 9
+; IIX  => WRONG!
+; VIII => 8
+
+; Write the function roman->int
 (unfinished roman->int)
 
+; Add test cases here and proceed using the top-down TDD methodology
 (fact
   (roman->int "I") => 1)
