b37db7971c5c08da57f018e908c97e4ff2e52744
Carlo Sciolla <skuro@skuro.tk>
2015-04-19 11:11:20 +0200

    Added midje dep; better initial failures for functions

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://www.sytac.nl"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :profiles {:dev {:dependencies [[midje "1.6.3"]]
+                   :plugins [[lein-midje "3.1.3"]]}})
--- test/intro/fun.clj
+++ test/intro/fun.clj
@@ -15,35 +15,39 @@
 (defn increment-all
   "Takes a sequence of numbers and returns another sequence with all values
    incremented by one"
-  [xs])
+  [xs]
+  _)
 
 (defn remove-odds
   "Takes a sequence of numbers and returns another sequence of all the even
    numbers"
-  [xs])
+  [xs]
+  _)
 
 (defn apply-twice
   "Takes a function of one argument f(x) and returns a function g so that
    g(x) = f(f(x))"
-  [f x])
+  [f x]
+  _)
 
 (defn apply-implicit-first
   "Takes a function of two arguments f(x, y) and a value x and returns a
    function g so that g(y) = f(x, y)"
-  [f x])
+  [f x]
+  _)
 
 (deftest basic-functions
   (testing "Write the body of the above functions so that they fulfill the tests"
     (are [expected result] (= expected result)
          [2 3 4 5] (increment-all [1 2 3 4])
          [2 4]     (remove-odds [1 2 3 4])
-         4         ((apply-twice inc) 2)
+         4         ((apply-twice inc 2))
          42        ((apply-implicit-first + 5) 37))))
 
 (deftest seq-processing-functions
   (testing "Explore the power of the Clojure standard library"
     (= ["1" "2" "3"] (map _ [1 2 3]))
-    (= _ (filter _ ))
+    (= _ (filter odd? [1 2 3]))
     (= _ (apply + [1 2 3]))
     (= _ (let [sum-of-chars _]
            (reduce sum-of-chars ["one" "two" "three"]))))
