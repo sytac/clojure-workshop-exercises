2dc5e274aabce72b4d0904d81bddcb24c4e633de
Carlo Sciolla <skuro@skuro.tk>
2015-04-20 18:31:06 +0200

    More tests

--- test/ooo/datatypes.clj
+++ test/ooo/datatypes.clj
@@ -24,3 +24,45 @@
       (perimeter (->Square 4))        => 16
       (wider-than (->Square 4)
                   (->Rectangle 4 2))) => true
+
+; Here is a protocol that implements a datastore
+(defprotocol Store
+  (put [this key data])
+  (get [this key]))
+
+; here's a set of functions that depend on the protocol
+(defn checked-add
+  "Accepts a store and a validation fn and it returns another function
+   that can add an element to the store if it passes the validation"
+  [store valid?]
+  (fn [store key data]
+    (if (valid? key data)
+      (put store key data)
+      false)))
+
+(defn get-or-default
+  "Accepts a default value and it returns another function
+   that will try to fetch an element from the store, or return the default
+   value if it's not found"
+  [default]
+  (fn [store key]
+    (if-let [data (get store key)]
+      data
+      default)))
+
+; implement a dummy store that will turn the following tests green
+; use first a Record, then reify
+(unfinished dummy-store)
+
+(defn key-map-checker
+  "Validator function that accepts a keyword as a key and a map as the data"
+  [key data]
+  (and (keyword? key)
+       (map? data)))
+
+(fact "Stores are made for storing"
+      ((checked-add key-map-checker) dummy-store :key {})       => {:key   {}
+                                                                    :store {:type :dummy}}
+      ((checked-add key-map-checker) dummy-store :key :val)     => false
+      ((get-or-default {:error :not-found}) dummy-store :store) => {:type :dummy}
+      ((get-or-default {:error :not-found}) dummy-store :woot)  => {:error :not-found})
--- test/ooo/multi.clj
+++ test/ooo/multi.clj
@@ -18,3 +18,21 @@
       (area {:type :rectangle :width 4 :length 2}) => 8
       (area {:type :square :width 4})              => 16
       (area {:type :circle :radius 1})             => (roughly 3.14 0.01))
+
+(defmulti animal-sound
+  "Allows an animal to say something"
+  :kind)
+
+(defmethod animal-sound ::dog
+  [this]
+  (str (:name this) " barks"))
+
+(defmethod animal-sound ::animal
+  [this]
+  (str (:name this) " emits a sound"))
+
+(fact "Use derive to make the following test pass"
+      (animal-sound {:type ::dog
+                     :name "Fido"}) =>  "Fido barks"
+      (animal-sound {:type ::cat
+                     :name "Mr. Wiskers"}) => "Mr. Wiskers emits a sound")
