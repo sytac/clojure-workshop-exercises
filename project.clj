2779e6f0997279e27a520d2fa87a57e7359b47e5
Carlo Sciolla <skuro@skuro.tk>
2015-04-21 07:14:33 +0200

    Added dependency

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://www.sytac.nl"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.typed "0.2.84"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]
-                                  [org.clojure/data.csv "0.1.2"]]
+                                  [org.clojure/data.csv "0.1.2"]
+                                  [org.clojure/tools.trace "0.7.8"]]
                    :plugins [[lein-midje "3.1.3"]]}})
